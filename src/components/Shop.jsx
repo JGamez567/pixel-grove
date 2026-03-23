04dd468d8cbd655cf803bdf67583ba7803128015
jgamez567 <jonathangamez10907@gmail.com>
2026-03-22 23:03:59 -0700

    database pics

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -107,7 +107,7 @@ function PetCard({ petName, combos }) {
       }}>
 
       <div className="rounded-xl p-3 mb-4 flex items-center justify-center h-36 relative" style={{ background: 'rgba(255,255,255,0.03)' }}>
-        <img src={combos[0].image_url} alt={petName} className="h-full object-contain" />
+        <img src={combos.find(c => c.image_url)?.image_url} alt={petName} className="h-full object-contain" />
         <div className="absolute top-2 right-2"><Badge type={selectedType} potion={selectedPotion} /></div>
         {rarityStyle && (
           <div className="absolute bottom-2 left-2">
